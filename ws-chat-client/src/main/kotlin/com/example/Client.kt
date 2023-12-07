245eb81d43080ca41e4d67aaab25fedf3ae5da5c
josealonso <jose.alonso.programmer@gmail.com>
2023-12-07 01:52:02 +0100

    The last code might cause code blocking. Two differente coroutines must be declared, as shown here.

--- ws-chat-client/src/main/kotlin/com/example/Client.kt
+++ ws-chat-client/src/main/kotlin/com/example/Client.kt
@@ -3,6 +3,8 @@ package com.example
 import io.ktor.client.*
 import io.ktor.client.plugins.websocket.*
 import io.ktor.websocket.*
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
@@ -12,15 +14,22 @@ fun main() {
 
     runBlocking {
         client.webSocket(port = 8080, path = "/chat") {
-            while (true) {
-                val text = readln()
-                send(text)
+            val outgoingMessages = launch {
+                while (true) {
+                    val text = readln()
+                    send(text)
+                }
+            }
 
+            val incomingMessages = launch {
                 for (frame in incoming) {
                     frame as? Frame.Text ?: continue
                     println(frame.readText())
                }
             }
+
+            outgoingMessages.join()
+            incomingMessages.cancelAndJoin()
         }
     }
 }
