3bda0dd08836fd0036163eaf649e7c62040dc845
josealonso <jose.alonso.programmer@gmail.com>
2023-12-07 01:39:14 +0100

    A suspend function needs to be executed from a proper context.

--- ws-chat-client/src/main/kotlin/com/example/Client.kt
+++ ws-chat-client/src/main/kotlin/com/example/Client.kt
@@ -2,13 +2,25 @@ package com.example
 
 import io.ktor.client.*
 import io.ktor.client.plugins.websocket.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.runBlocking
 
 fun main() {
     val client = HttpClient {
         install(WebSockets)
     }
 
-    client.webSocket(port = 8080, path = "/chat") {
+    runBlocking {
+        client.webSocket(port = 8080, path = "/chat") {
+            while (true) {
+                val text = readln()
+                send(text)
 
+                for (frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    println(frame.readText())
+               }
+            }
+        }
     }
 }
